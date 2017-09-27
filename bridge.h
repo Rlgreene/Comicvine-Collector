87e19e1aca3a0b2839d9cf523fc59aefdbf2d932
Richard Greene <RichardGreene@Richards-MacBook-Pro.local>
2017-09-27 18:58:42 -0400

    good so far

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  Comicvinetest
+//
+//  Created by Richard Greene on 9/27/17.
+//  Copyright © 2017 Richard Greene. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "AFNetworking/AFNetworking.h"
+
+#endif /* bridge_h */
